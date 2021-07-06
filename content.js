b92620c8857366f9965888b94f2582063f6379ca
Akash Rana <akashrana20022000@gmail.com>
2021-07-07 00:08:35 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,25 @@
+var urlPage = window.location.toString();
+urlPage = urlPage.replace("#",'')
+var array = urlPage.split("/");
+
+fileName = array[array.length -2] + array[array.length-1] + "-codeforces" + ".cpp"
+
+// get the title div from Codeforces.com
+var el1 = document.querySelector(".title")
+
+var downloadLink = document.createElement('a');
+downloadLink.innerHTML = "Code"
+downloadLink.style.marginLeft = "24px"
+downloadLink.style.textDecoration = "none"
+
+el1.appendChild(downloadLink)
+
+// make a text-file
+var blob = new Blob([], {type: "text/plain;charset=utf-8"});
+
+const link = URL.createObjectURL(blob);
+
+downloadLink.href = link
+downloadLink.setAttribute("download", fileName); // Choose the file name
+
+    
